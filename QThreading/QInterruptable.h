fab150672cbfdab3ac24fc9e75ee801092126d3b
KGCybeX <k4gcybex@gmail.com>
2019-10-14 09:46:53 +0200

    Defined QInterruptable interface

--- QThreading/QInterruptable.h
+++ QThreading/QInterruptable.h
@@ -1,4 +1,14 @@
 #ifndef QINTERRUPTABLE_H
 #define QINTERRUPTABLE_H
 
+class QInterruptable {
+public:
+    virtual void pause();
+    virtual void resume();
+    virtual void interrupt();
+    virtual ~QInterruptable(){
+
+    }
+};
+
 #endif // QINTERRUPTABLE_H
